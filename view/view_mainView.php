481e48a0d1e388c2fd596ae9ca4dc5e736aa0db2
koriley <kevin@blueshorestudios.com>
2016-03-07 11:12:49 -0600

    Removed inline style created a top class to pull the top image to the top

--- view/view_mainView.php
+++ view/view_mainView.php
@@ -22,20 +22,20 @@ echo "<script> var userID='$id'; </script>";
 ?>
 <div id="edit" class="adminEdit"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span>edit</div>
 <!-- this is the ads above the form-->
-<div class="headImage" style="text-align: center; max-width:400px; margin:auto; padding-bottom:15px;">
-    <img style="width:100% " src="/img/Fashionation16_WebAppHeader2.png" />
+<div class="headImage top" >
+    <img  src="/img/Fashionation16_WebAppHeader2.png" />
 </div>
 <div class="welcomText" style="text-align: center; max-width:400px; margin:auto;">
     <span style='font-weight:bold'>WELCOME TO THE #417FASHIONATION STYLE GUIDE!</span><br/>
     Follow along with the runway show and click to star the fashions you love below. We'll email you everything you liked at the end of the night!
 </div>
-<div class="headImage" style='max-width:400px; margin:auto; padding-top:15px;'>
-    <img style="width:100% " src="/img/Fashionation16_WebAppSponsors.png" />
+<div class="headImage" >
+    <img  src="/img/Fashionation16_WebAppSponsors.png" />
 </div>
-<div class="headImage" style='max-width:400px; margin:auto; padding-top:15px;'>
-    <img style="width:100% " src="/img/Fashionation16_WebApp_BlackBar.png" />
+<div class="headImage" >
+    <img  src="/img/Fashionation16_WebApp_BlackBar.png" />
 </div>
-<div id='userView'></div>
+<div id='userView' class="userView"></div>
 <script>
     jQuery('#userView').load('control/control_getModelsMainView.php?id='+userID);
 </script>
