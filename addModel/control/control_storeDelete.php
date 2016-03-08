6a00e8187769594376eb3bc8115f2ca073a74839
koriley <kevin@blueshorestudios.com>
2016-03-08 12:29:34 -0600

    control to delete a store

---
+++ addModel/control/control_storeDelete.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * control to delete the store
+ */
+
+function __autoload($class_name) {
+    include '../modules/module_' . $class_name . '.php';
+}
+
+include_once('../../inc/dbl.login.inc.php');
+$dbCon = new PDODatabase($dbAdmin, $dbName, $dbPass);
+$addModel = new model();
+
+$storeDelete = $_POST['deleteStore'];
+$image1 = $_POST['delImage1'];
+$image2 = $_POST['delImage2'];
+
+$res = $addModel->deleteStore($dbCon, $storeDelete, $image1, $image2);
