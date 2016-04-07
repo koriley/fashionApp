e6e8829ceb437381b69eda1962a12f1870c8460f
koriley <kevin@blueshorestudios.com>
2016-04-07 12:45:23 -0500

    created a new menu that is touch sensitive and click sensitive

--- view/view_mainView.php
+++ view/view_mainView.php
@@ -23,11 +23,11 @@ echo "<script> var userID='$id'; </script>";
 <div class="headImage top" >
     <img  src="/img/Fashionation16_WebAppHeader8.png" />
 </div>
-<div class="menu" id="menu" style="">
-    <a href="#menu"><img src="/img/menuIcon.png" /></a>
+<div class="menu" style="">
+    <div id="menu"><img src="/img/menuIcon.png" /></div>
 </div>
 <div class="welcomText" style="">
-Follow along with the show and click to star the fashions you love below. We'll save everything as you go and email you what you liked at the end of the night!
+    Follow along with the show and click to star the fashions you love below. We'll save everything as you go and email you what you liked at the end of the night!
 </div>
 <div class="headImage" >
     <img  src="/img/Fashionation16_WebAppSponsors.png" />
@@ -41,11 +41,23 @@ Follow along with the show and click to star the fashions you love below. We'll 
 
 <script type="text/javascript">
 
-    
+    jQuery('.menu').on('touch click', function () {
+        //alert('click');
+        jQuery('.greyOut').toggle();
+        jQuery('.menuItems').toggle();
+    });
+    jQuery('.greyOut').on('touch click', function () {
+        jQuery('.greyOut').toggle();
+        jQuery('.menuItems').toggle();
+    });
+    jQuery('a').on('touch click', function(){
+        jQuery('.greyOut').toggle();
+        jQuery('.menuItems').toggle();
+    });
 
 
 
     jQuery('#userView').load('control/control_getModelsMainView.php?id=' + userID);
-    
+
 </script>
 
